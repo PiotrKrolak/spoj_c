44aab6e698013787014adc82bd284fccfa85af83
PiotrKrolak <krolak.piotr.lca@gmail.com>
2022-06-10 23:00:11 +0200

    zadanie spoj - PTROL

---
+++ PTROL/PTROL.c
@@ -0,0 +1,17 @@
+// spoj exercise https://pl.spoj.com/problems/PTROL/
+
+#include <stdio.h>
+
+int ptrol(int n);
+
+int main(){
+
+    return 0;
+}
+
+int ptrol(int n){
+    for(n, n>0; n--){
+
+
+    }
+}
